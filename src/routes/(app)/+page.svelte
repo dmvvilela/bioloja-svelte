d6f1df7939e9206495bb5064021da8222249adac
Daniel Vilela <dmvvilela@gmail.com>
2023-08-18 20:45:13 -0300

    add tailwind and fonts

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Bioloja: Materiais Didáticos de Biologia</title>
+	<meta
+		name="description"
+		content="Materiais Didáticos de Biologia. Aula prontas para o ensino médio e superior."
+	/>
+</svelte:head>
