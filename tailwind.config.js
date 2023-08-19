4784c0211e4fd4896abec6602b92c17106306bd6
Daniel Vilela <dmvvilela@gmail.com>
2023-08-18 22:32:37 -0300

    images and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,37 @@ export default {
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
-		themes: ['cupcake'], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		// darkTheme: "dark", // name of one of the included themes for dark mode
-		base: true, // applies background color and foreground color for root element by default
-		styled: true, // include daisyUI colors and design decisions for all components
-		utils: true, // adds responsive and modifier utility classes
-		rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
-		prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-		logs: true // Shows info about daisyUI version and used config in the console when building your CSS
+		themes: [
+			{
+				// Check: https://daisyui.com/docs/colors/ for all color names
+				cupcake: {
+					// eslint-disable-next-line @typescript-eslint/no-var-requires
+					...require('daisyui/src/theming/themes')['[data-theme=cupcake]'],
+					primary: '#7C9AA6'
+				}
+			}
+		]
 	}
 };
+
+// Canva
+// Tiber #092A39
+// Gumbo #7B94A3
+// Pearl Bush #E8E2D4
+// Slate Gray #778C9C
+
+// Canva 2
+// Sisal #D2CDBE
+
+// Adobe 1
+// #012E40
+// #7C9AA6
+// #2F5559
+// #F2BC79
+// #F2F2F2
+
+// Adobe 2
+// #025949
+
+// Medidor Digital
+// #00594E
