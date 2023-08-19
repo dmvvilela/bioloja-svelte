c7870232ee0ea0b804120db18f015eaa82f65b38
Daniel Vilela <dmvvilela@gmail.com>
2023-08-18 21:05:49 -0300

    code from old bioloja-site

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,55 @@
 <script>
-	import '../app.css';
+	import Footer from '$lib/components/layout/footer.svelte';
+	import Header from '$lib/components/layout/header.svelte';
+	import '../../app.css';
+
+	const title = 'Bioloja - Materiais Didáticos de Biologia';
+	const description =
+		'Aulas, apostilas, exercícios e simulados de Biologia para os Ensinos Fundamental/Médio/Superior e o Enem. Tanto para professores quanto alunos.';
 </script>
 
-<slot />
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta name="author" content="Prof. Dr. Ana Luisa Miranda-Vilela" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<!-- <meta
+		property="og:image"
+		content={'https://firebasestorage.googleapis.com/v0/b/anatomia-e-fisiologia-humanas.appspot.com/o/AFH_reduzida_principal.png?alt=media&token=3c661a73-463c-4fcd-a274-bc25a24cfdec'}
+	/> -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<!-- <meta
+		property="twitter:image"
+		content={'https://firebasestorage.googleapis.com/v0/b/anatomia-e-fisiologia-humanas.appspot.com/o/AFH_reduzida_principal.png?alt=media&token=3c661a73-463c-4fcd-a274-bc25a24cfdec'}
+	/> -->
+	<!-- https://kit.svelte.dev/docs/seo -->
+	<!-- A common pattern is to return SEO-related data from page load functions, then use it (as $page.data) in a <svelte:head> in your root layout. -->
+</svelte:head>
+
+<div class="relative" data-sveltekit-preload-data="tap" data-sveltekit-preload-code="hover">
+	<Header />
+
+	<!-- <Searchbox isOpen={isSearchOpen} {toggleSearch} /> -->
+	<!-- {#if $toasts}
+		<section class="fixed right-2.5 z-[999] mt-2.5 flex flex-col justify-center">
+			{#each $toasts as toast (toast.id)}
+				<Toast
+					type={toast.type}
+					dismissible={toast.dismissible}
+					on:dismiss={() => dismissToast(toast.id)}
+				>
+					{toast.message}
+				</Toast>
+			{/each}
+		</section>
+	{/if} -->
+
+	<main>
+		<slot />
+	</main>
+
+	<Footer />
+</div>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,7 @@
-<svelte:head>
-	<title>Bioloja: Materiais Didáticos de Biologia</title>
-	<meta
-		name="description"
-		content="Materiais Didáticos de Biologia. Aula prontas para o ensino médio e superior."
-	/>
-</svelte:head>
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
