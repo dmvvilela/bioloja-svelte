f445906eaee27b7f6b1240925407cb00ed20fe7a
Daniel Vilela <dmvvilela@gmail.com>
2024-04-03 14:31:49 -0300

    fix conn env

--- src/lib/server/db/conn.ts
+++ src/lib/server/db/conn.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
+import { PG_CONN } from '$env/static/private';
 // import * as schema from './schema';
 
-const sql = neon(Bun.env.PG_CONN || '');
+const sql = neon(PG_CONN);
 export const db = drizzle(sql);
