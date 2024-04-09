cddb65ea1b90f12e50fe00203aaae219d363e842
Daniel Vilela <dmvvilela@gmail.com>
2024-04-09 16:42:27 -0300

    fix dynamic import

--- src/lib/server/mail/mail.ts
+++ src/lib/server/mail/mail.ts
@@ -38,17 +38,17 @@ export const templateNameToSubject = (template: string) => {
 };
 
 export const getTemplateComponent = async (template: string, type: 'mjml' | 'svelte') => {
-	return (await import(`/src/lib/emails/${type}/${template}.svelte`)).default;
+	return (await import(`../../emails/${type}/${template}.svelte`)).default;
 };
 
 export const renderEmailBody = async (
-	templateComponent: any,
+	template: string,
 	subject: string,
 	type: 'mjml' | 'svelte',
 	props?: Record<string, unknown>
 ) => {
 	try {
-		// const templateComponent = await getTemplateComponent(template, type);
+		const templateComponent = await getTemplateComponent(template, type);
 
 		if (type === 'mjml') {
 			// Render the email template to html and text
--- src/routes/(storefront)/admin/email/preview/+page.server.ts
+++ src/routes/(storefront)/admin/email/preview/+page.server.ts
@@ -5,9 +5,7 @@ export const load: PageServerLoad = async ({ url }) => {
 	const type = url.searchParams.get('type') as 'mjml' | 'svelte';
 	const template = url.searchParams.get('template') as string;
 
-	const templateComponent = (await import(`/src/lib/emails/${type}/${template}.svelte`)).default;
-
-	const email = await renderEmailBody(templateComponent, 'E-mail Preview', type, {
+	const email = await renderEmailBody(template, 'E-mail Preview', type, {
 		// This is type-checked!
 		prop: 'Test'
 	});
