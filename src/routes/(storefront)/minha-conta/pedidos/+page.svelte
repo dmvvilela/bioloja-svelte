d27a099136696c1d8b0899c650ef35d3b93166f6
Daniel Vilela <dmvvilela@gmail.com>
2024-04-10 20:43:16 -0300

    no orders ui

--- src/routes/(storefront)/minha-conta/pedidos/+page.svelte
+++ src/routes/(storefront)/minha-conta/pedidos/+page.svelte
@@ -4,6 +4,7 @@
 	// export let data: PageData;
 
 	// const  user = data.user!;
+	const orders = [];
 </script>
 
 <div class="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow mt-3">
@@ -12,80 +13,86 @@
 		<p class="font- text-slate-600">Verifique seus pedidos anteriores.</p>
 	</div>
 	<hr class="mt-4 mb-8" />
-	<div class="overflow-x-auto">
-		<table class="table">
-			<!-- head -->
-			<thead>
-				<tr>
-					<th />
-					<th>DATA</th>
-					<th>STATUS</th>
-					<th>TOTAL</th>
-					<th />
-				</tr>
-			</thead>
-			<tbody>
-				<!-- row 1 -->
-				<tr>
-					<th>#1</th>
-					<td>20/04/2024</td>
-					<td><span class="badge badge-success badge-sm">Concluído</span></td>
-					<td>R$80,27 (3 items)</td>
-					<th>
-						<button class="btn btn-xs btn-primary">Ver pedido</button>
-					</th>
-				</tr>
-				<!-- row 2 -->
-				<tr>
-					<th>#2</th>
-					<td>21/04/2024</td>
-					<td><span class="badge badge-ghost badge-sm">Pagamento Pendente</span></td>
-					<td>R$8,21 (1 item)</td>
-					<th>
-						<button class="btn btn-xs btn-primary">Ver pedido</button>
-					</th>
-				</tr>
-				<!-- row 3 -->
-				<tr>
-					<th>#3</th>
-					<td>22/04/2024</td>
-					<td><span class="badge badge-outline badge-sm">Processando</span></td>
-					<td>R$42,30 (2 items)</td>
-					<th>
-						<button class="btn btn-xs btn-primary">Ver pedido</button>
-					</th>
-				</tr>
-				<!-- row 4 -->
-				<tr>
-					<th>#2</th>
-					<td>21/04/2024</td>
-					<td><span class="badge badge-error badge-sm">Cancelado</span></td>
-					<td>R$8,21 (1 item)</td>
-					<th>
-						<button class="btn btn-xs btn-primary">Ver pedido</button>
-					</th>
-				</tr>
-				<!-- row 5 -->
-				<tr>
-					<th>#2</th>
-					<td>21/04/2024</td>
-					<td><span class="badge badge-warning badge-sm">Aguardando</span></td>
-					<td>R$8,21 (1 item)</td>
-					<th>
-						<button class="btn btn-xs btn-primary">Ver pedido</button>
-					</th>
-				</tr>
-				<!-- row 5 -->
-				<tr>
-					<th>#2</th>
-					<td>21/04/2024</td>
-					<td><span class="badge badge-info badge-sm">Reembolsado</span></td>
-					<td>R$8,21 (1 item)</td>
-					<th>
-						<button class="btn btn-xs btn-primary">Ver pedido</button>
-					</th>
-				</tr>
-			</tbody>
-		</table>
-	</div>
+	{#if orders.length == 0}
+		<div class="mb-8">
+			<p class="text-gray-600 text-sm mb-7 italic">Você ainda não realizou nenhum pedido.</p>
+			<a href="/loja">
+				<button
+					class="btn btn-primary btn-md glass bg-primary-focus text-base border border-primary text-white px-8 font-medium
+				rounded-md hover:shadow-lg">Comprar Agora</button
+				>
+			</a>
+		</div>
+	{/if}
+	{#if orders.length > 0}
+		<div class="overflow-x-auto mb-4">
+			<table class="table">
+				<thead>
+					<tr>
+						<th />
+						<th>DATA</th>
+						<th>STATUS</th>
+						<th>TOTAL</th>
+						<th />
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>#1</th>
+						<td>20/04/2024</td>
+						<td><span class="badge badge-success badge-sm">Concluído</span></td>
+						<td>R$80,27 (3 items)</td>
+						<th>
+							<button class="btn btn-xs btn-primary">Ver pedido</button>
+						</th>
+					</tr>
+					<tr>
+						<th>#2</th>
+						<td>21/04/2024</td>
+						<td><span class="badge badge-ghost badge-sm">Pagamento Pendente</span></td>
+						<td>R$8,21 (1 item)</td>
+						<th>
+							<button class="btn btn-xs btn-primary">Ver pedido</button>
+						</th>
+					</tr>
+					<tr>
+						<th>#3</th>
+						<td>22/04/2024</td>
+						<td><span class="badge badge-outline badge-sm">Processando</span></td>
+						<td>R$42,30 (2 items)</td>
+						<th>
+							<button class="btn btn-xs btn-primary">Ver pedido</button>
+						</th>
+					</tr>
+					<tr>
+						<th>#2</th>
+						<td>21/04/2024</td>
+						<td><span class="badge badge-error badge-sm">Cancelado</span></td>
+						<td>R$8,21 (1 item)</td>
+						<th>
+							<button class="btn btn-xs btn-primary">Ver pedido</button>
+						</th>
+					</tr>
+					<tr>
+						<th>#2</th>
+						<td>21/04/2024</td>
+						<td><span class="badge badge-warning badge-sm">Aguardando</span></td>
+						<td>R$8,21 (1 item)</td>
+						<th>
+							<button class="btn btn-xs btn-primary">Ver pedido</button>
+						</th>
+					</tr>
+					<tr>
+						<th>#2</th>
+						<td>21/04/2024</td>
+						<td><span class="badge badge-info badge-sm">Reembolsado</span></td>
+						<td>R$8,21 (1 item)</td>
+						<th>
+							<button class="btn btn-xs btn-primary">Ver pedido</button>
+						</th>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	{/if}
 </div>
