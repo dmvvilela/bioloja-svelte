e33d6c54979dd94467ab1c62e756ca4c9b0802b5
Daniel Vilela <dmvvilela@gmail.com>
2024-04-10 19:39:19 -0300

    layout on my account

---
+++ src/routes/(storefront)/minha-conta/+layout.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { invalidateAll } from '$app/navigation';
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+
+	$: path = $page.route.id;
+	const { user } = data;
+
+	let menuOpen = false;
+
+	const logOut = async () => {
+		await fetch('/sair', { method: 'POST' });
+		invalidateAll();
+	};
+
+	const menus = [
+		{
+			title: 'Detalhes',
+			path: '/minha-conta'
+		},
+		{
+			title: 'Pedidos',
+			path: '/minha-conta/pedidos'
+		},
+		{
+			title: 'Downloads',
+			path: '/minha-conta/downloads'
+		}
+	];
+
+	const classes = (menu: any) =>
+		path?.endsWith(menu.path) ? 'border-l-primary text-primary' : 'border-transparent';
+</script>
+
+<div class="mx-4 max-w-screen-xl py-16 mb-10 sm:mx-8 xl:mx-auto">
+	<h1 class="border-b py-6 text-4xl font-semibold">Minha Conta</h1>
+	<div class="grid grid-cols-8 pt-3 sm:grid-cols-10">
+		<div class="relative my-4 w-56 sm:hidden">
+			<label
+				for="select-1"
+				class="flex w-full cursor-pointer select-none rounded-lg border p-2 px-3 text-sm text-gray-700 ring-primary peer-checked:ring"
+				>Detalhes
+			</label>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="pointer-events-none absolute right-0 top-3 ml-auto mr-5 h-4 text-slate-700 transition peer-checked:rotate-180"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="2"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+			</svg>
+			<ul
+				class="max-h-0 select-none flex-col overflow-hidden rounded-b-lg shadow-md transition-all duration-300 peer-checked:max-h-56 peer-checked:py-3"
+			>
+				{#each menus as menu}
+					<li
+						class="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-primary hover:text-white"
+					>
+						{menu.title}
+					</li>
+				{/each}
+			</ul>
+		</div>
+
+		<div class="col-span-2 hidden sm:block text-secondary">
+			<ul>
+				{#each menus as menu}
+					<li
+						class="{classes(
+							menu
+						)} mt-5 cursor-pointer border-l-2 px-2 py-1.5 my-0.5 font-semibold transition hover:border-l-primary hover:text-primary"
+					>
+						{menu.title}
+					</li>
+				{/each}
+				<li
+					class="text-red-500 mt-5 cursor-pointer border-l-2 border-transparent px-2 py-1 my-1 font-semibold transition hover:border-l-red-500"
+				>
+					<button on:click={logOut}>Sair</button>
+				</li>
+			</ul>
+		</div>
+		<slot />
+	</div>
+</div>
--- src/routes/(storefront)/minha-conta/+page.svelte
+++ src/routes/(storefront)/minha-conta/+page.svelte
@@ -1,122 +1,38 @@
 <script lang="ts">
-	import { invalidateAll } from '$app/navigation';
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
 
 	const { user } = data;
-
-	let menuOpen = false;
-
-	const logOut = async () => {
-		console.log('Saindo...');
-		await fetch('/sair', { method: 'POST' });
-		invalidateAll();
-	};
 </script>
 
-<div class="mx-4 max-w-screen-xl py-16 mb-10 sm:mx-8 xl:mx-auto">
-	<h1 class="border-b py-6 text-4xl font-semibold">Minha Conta</h1>
-	<div class="grid grid-cols-8 pt-3 sm:grid-cols-10">
-		<div class="relative my-4 w-56 sm:hidden">
-			<label
-				for="select-1"
-				class="flex w-full cursor-pointer select-none rounded-lg border p-2 px-3 text-sm text-gray-700 ring-primary peer-checked:ring"
-				>Detalhes
-			</label>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="pointer-events-none absolute right-0 top-3 ml-auto mr-5 h-4 text-slate-700 transition peer-checked:rotate-180"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-				stroke-width="2"
-			>
-				<path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-			</svg>
-			<ul
-				class="max-h-0 select-none flex-col overflow-hidden rounded-b-lg shadow-md transition-all duration-300 peer-checked:max-h-56 peer-checked:py-3"
-			>
-				<li
-					class="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-primary hover:text-white"
-				>
-					Detalhes
-				</li>
-				<li
-					class="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-primary hover:text-white"
-				>
-					Pedidos
-				</li>
-				<li
-					class="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-primary hover:text-white"
-				>
-					Downloads
-				</li>
-				<li
-					class="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-primary hover:text-white"
-				>
-					Sair
-				</li>
-			</ul>
-		</div>
-
-		<div class="col-span-2 hidden sm:block text-secondary">
-			<ul>
-				<li
-					class="mt-5 cursor-pointer border-l-2 border-l-primary px-2 py-2 font-semibold text-primary transition hover:border-l-primary hover:text-primary"
-				>
-					Detalhes
-				</li>
-				<li
-					class="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-primary hover:text-primary"
-				>
-					Pedidos
-				</li>
-				<li
-					class="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-primary hover:text-primary"
-				>
-					Downloads
-				</li>
-				<!-- <li
-					class="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-primary hover:text-primary"
-				>
-					Endereços
-				</li> -->
-				<li
-					class="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition text-red-500 hover:border-l-primary hover:text-primary"
-				>
-					Sair
-				</li>
-			</ul>
-		</div>
-
-		<div class="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow mt-3">
-			<div class="pt-4">
-				<h1 class="py-2 text-2xl font-semibold">Detalhes da conta</h1>
-				<!-- <p class="font- text-slate-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> -->
-			</div>
-			<hr class="mt-4 mb-8" />
-			<p class="py-2 text-xl font-semibold">E-mail</p>
-			<div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-				<p class="text-gray-600">Seu e-mail é <strong>{user.email}</strong>.</p>
-			</div>
-			<hr class="mt-4 mb-8" />
-			<p class="py-2 text-xl font-semibold">Nome</p>
-			<div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-				<p class="text-accent">Seu nome é <strong>{user.name}</strong>.</p>
-				<!-- <button class="inline-flex text-sm font-semibold text-bioloja-600 underline decoration-2"
+<div class="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow mt-3">
+	<div class="pt-4">
+		<h1 class="py-2 text-2xl font-semibold">Detalhes da conta</h1>
+		<!-- <p class="font- text-slate-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> -->
+	</div>
+	<hr class="mt-4 mb-8" />
+	<p class="py-2 text-xl font-semibold">E-mail</p>
+	<div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+		<p class="text-gray-600">Seu e-mail é <strong>{user.email}</strong>.</p>
+	</div>
+	<hr class="mt-4 mb-8" />
+	<p class="py-2 text-xl font-semibold">Nome</p>
+	<div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+		<p class="text-gray-600">Seu nome é <strong>{user.name}</strong>.</p>
+		<!-- <button class="inline-flex text-sm font-semibold text-bioloja-600 underline decoration-2"
 					>Editar</button
 				> -->
-			</div>
-			<hr class="mt-4 mb-8" />
-			<p class="py-2 text-xl font-semibold">Senha</p>
-			<div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-				<p class="text-accent">
-					Caso queira alterar sua senha, clique em Sair para deslogar da sua conta e clique no link
-					"esqueci a senha" na tela de login e siga as instruções indicadas.
-				</p>
-			</div>
-			<!-- <div class="flex items-center">
+	</div>
+	<hr class="mt-4 mb-8" />
+	<p class="py-2 text-xl font-semibold">Senha</p>
+	<div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+		<p class="text-accent">
+			Caso queira alterar sua senha, clique em Sair para deslogar da sua conta, em seguida clique no
+			link "esqueci a senha" na tela de login e siga as instruções indicadas.
+		</p>
+	</div>
+	<!-- <div class="flex items-center">
 				<div class="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
 					<label for="login-password">
 						<span class="text-sm text-gray-500">Current Password</span>
@@ -170,9 +86,9 @@
 				class="mt-4 rounded-lg bg-primary px-4 py-2 text-white font-medium hover:bg-bioloja-700"
 				>Save Password</button
 			> -->
-			<!-- <hr class="mt-4 mb-8" /> -->
-			<div class="mb-8" />
-			<!-- <div class="mb-10">
+	<!-- <hr class="mt-4 mb-8" /> -->
+	<div class="mb-8" />
+	<!-- <div class="mb-10">
 				<p class="py-2 text-xl font-semibold">Delete Account</p>
 				<p class="inline-flex items-center rounded-full bg-rose-100 px-4 py-1 text-rose-600">
 					<svg
@@ -198,6 +114,4 @@
 					>Continue with deletion</button
 				>
 			</div> -->
-		</div>
-	</div>
 </div>
