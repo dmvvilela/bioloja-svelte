953e99adb99bc3587114c61d50dece7876668683
Daniel Vilela <dmvvilela@gmail.com>
2024-04-11 09:32:48 -0300

    finish order ui

--- src/routes/(storefront)/minha-conta/pedidos/[order]/+page.svelte
+++ src/routes/(storefront)/minha-conta/pedidos/[order]/+page.svelte
@@ -2,10 +2,41 @@
 	import type { PageData } from './$types';
 	import slide1 from '$lib/images/slides1/Slide1.jpg';
 	import slide2 from '$lib/images/slides1/Slide2.jpg';
+	import slide3 from '$lib/images/slides1/Slide3.jpg';
+	import { getLocalePrice } from '$lib/utils/product';
 
 	// export let data: PageData;
 
 	// const  user = data.user!;
+
+	const products = [
+		{
+			id: 1,
+			category: 'Botânica, Seres Vivos',
+			name: 'Algas',
+			slug: 'product-1',
+			image: slide1,
+			price: 1299,
+			discountPrice: 1099
+		},
+		{
+			id: 2,
+			category: 'Genética',
+			name: 'Homeostase',
+			slug: 'product-2',
+			image: slide2,
+			price: 2100,
+			discountPrice: 1599
+		},
+		{
+			id: 3,
+			category: 'Promoções, Anatomia e Fisiologia Humana',
+			name: 'Mega nome grande que deveria atrapalhar tudo mas mesmo assim funciona pq eu sou foda',
+			slug: 'product-3',
+			image: slide3,
+			price: 4230
+		}
+	];
 </script>
 
 <div class="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow mt-3">
@@ -22,66 +53,43 @@
 		<h2 class="sr-only">Resumo do pedido</h2>
 		<h3 class="sr-only">Items</h3>
 		<div>
-			<div class="flex space-x-6 border-b border-gray-200 py-4">
-				<img
-					src={slide1}
-					alt="Glass bottle with black plastic pour top and mesh insert."
-					class="h-20 w-20 flex-none rounded-lg bg-gray-100 object-cover object-center sm:h-40 sm:w-40"
-				/>
-				<div class="flex flex-auto flex-col">
-					<div>
-						<h4 class="font-medium text-gray-900">
-							<a href="#">Cold Brew Bottle</a>
-						</h4>
-						<p class="mt-2 text-sm text-gray-600">
-							This glass bottle comes with a mesh insert for steeping tea or cold-brewing coffee.
-							Pour from any angle and remove the top for easy cleaning.
-						</p>
-					</div>
-					<div class="mt-6 flex flex-1 items-end">
-						<dl class="flex space-x-4 divide-x divide-gray-200 text-sm sm:space-x-6">
-							<div class="flex">
-								<dt class="font-medium text-gray-900">Quantity</dt>
-								<dd class="ml-2 text-gray-700">1</dd>
+			{#each products as product}
+				<div class="flex space-x-6 border-b border-gray-200 py-4">
+					<img
+						src={slide1}
+						alt={product.name + ' capa'}
+						class="h-24 w-24 flex-none object-contain object-center sm:h-36 sm:w-36"
+					/>
+					<div class="flex flex-auto flex-col">
+						<div>
+							<h4 class="font-medium text-gray-900 max-w-lg">
+								<a
+									href="/loja/produto/{product.slug}"
+									class="font-semibold text-lg leading-tight uppercase text-gray-700 hover:text-gray-800"
+									>{product.name}</a
+								>
+							</h4>
+							<div class="mt-1">
+								<p class="text-gray-400 text-sm">{product.category}</p>
 							</div>
-							<div class="flex pl-4 sm:pl-6">
-								<dt class="font-medium text-gray-900">Price</dt>
-								<dd class="ml-2 text-gray-700">$32.00</dd>
-							</div>
-						</dl>
-					</div>
-				</div>
-			</div>
-			<div class="flex space-x-6 border-b border-gray-200 py-4">
-				<img
-					src={slide2}
-					alt="Glass bottle with black plastic pour top and mesh insert."
-					class="h-20 w-20 flex-none rounded-lg bg-gray-100 object-cover object-center sm:h-40 sm:w-40"
-				/>
-				<div class="flex flex-auto flex-col">
-					<div>
-						<h4 class="font-medium text-gray-900">
-							<a href="#">Cold Brew Bottle</a>
-						</h4>
-						<p class="mt-2 text-sm text-gray-600">
-							This glass bottle comes with a mesh insert for steeping tea or cold-brewing coffee.
-							Pour from any angle and remove the top for easy cleaning.
-						</p>
-					</div>
-					<div class="mt-6 flex flex-1 items-end">
-						<dl class="flex space-x-4 divide-x divide-gray-200 text-sm sm:space-x-6">
-							<div class="flex">
-								<dt class="font-medium text-gray-900">Quantity</dt>
-								<dd class="ml-2 text-gray-700">1</dd>
-							</div>
-							<div class="flex pl-4 sm:pl-6">
-								<dt class="font-medium text-gray-900">Price</dt>
-								<dd class="ml-2 text-gray-700">$32.00</dd>
-							</div>
-						</dl>
+						</div>
+						<div class="mt-6 flex flex-1 items-end">
+							<dl class="flex space-x-4 divide-x divide-gray-200 text-sm sm:space-x-6">
+								<div class="flex items-baseline mb-1 space-x-2 mt-1">
+									<p class="text-xl text-primary font-semibold">
+										R${getLocalePrice(product.discountPrice || product.price)}
+									</p>
+									{#if product.discountPrice}
+										<p class="text-sm text-gray-400 line-through">
+											R${getLocalePrice(product.price)}
+										</p>
+									{/if}
+								</div>
+							</dl>
+						</div>
 					</div>
 				</div>
-			</div>
+			{/each}
 		</div>
 
 		<div class="sm:ml-40 sm:pl-6">
@@ -89,7 +97,7 @@
 			<h4 class="sr-only">Payment</h4>
 			<dl class="grid grid-cols-2 gap-x-6 border-t border-gray-200 py-5 text-sm">
 				<div>
-					<dt class="font-medium text-gray-900">Payment method</dt>
+					<dt class="font-medium text-gray-900">Método de pagamento</dt>
 					<dd class="mt-2 text-gray-700">
 						<p>Apple Pay</p>
 						<p>Mastercard</p>
@@ -97,15 +105,16 @@
 					</dd>
 				</div>
 				<div>
-					<dt class="font-medium text-gray-900">Shipping method</dt>
+					<dt class="font-medium text-gray-900">Endereço de cobrança</dt>
 					<dd class="mt-2 text-gray-700">
-						<p>DHL</p>
-						<p>Takes up to 3 working days</p>
+						<p>SQN 211 Bl J</p>
+						<p>70863-100</p>
+						<p>Brasília / DF</p>
 					</dd>
 				</div>
 			</dl>
 
-			<h3 class="sr-only">Summary</h3>
+			<h3 class="sr-only">Resumo do pedido</h3>
 
 			<dl class="space-y-2 border-t border-gray-200 pt-6 text-sm">
 				<div class="flex justify-between">
@@ -114,20 +123,17 @@
 				</div>
 				<div class="flex justify-between">
 					<dt class="flex font-medium text-gray-900">
-						Discount
-						<span class="ml-2 rounded-full bg-gray-200 px-2 py-0.5 text-xs text-gray-600"
-							>STUDENT50</span
+						Desconto
+						<span class="ml-1.5 mt-0.5 rounded-full bg-gray-200 px-2 h-4 text-xs text-gray-600"
+							>BIOLOJANOTA10</span
 						>
 					</dt>
-					<dd class="text-gray-700">-$18.00 (50%)</dd>
+					<dd class="text-gray-700">-R$18,00</dd>
 				</div>
-				<div class="flex justify-between">
-					<dt class="font-medium text-gray-900">Shipping</dt>
-					<dd class="text-gray-700">$5.00</dd>
-				</div>
-				<div class="flex justify-between">
+
+				<div class="flex justify-between pt-2">
 					<dt class="font-medium text-gray-900">Total</dt>
-					<dd class="text-gray-900">$23.00</dd>
+					<dd class="text-gray-900">R$23,00</dd>
 				</div>
 			</dl>
 		</div>
