0a23134161d6dbcbc17ad2e5f1bbd893ed76f5e4
Daniel Vilela <dmvvilela@gmail.com>
2024-04-21 23:31:56 -0300

    sitemap.xml

---
+++ src/routes/api/routes/+server.ts
@@ -0,0 +1,45 @@
+import { error, json } from '@sveltejs/kit';
+import { db } from '$lib/server/db/conn';
+import { categories, products } from '$lib/server/db/schema';
+import { alias } from 'drizzle-orm/pg-core';
+import { eq } from 'drizzle-orm';
+import type { RequestHandler } from './$types';
+
+export const GET = (async () => {
+	const routes: string[] = [];
+
+	try {
+		// Query the database for all product slugs
+		const productSlugs = await db.select({ slug: products.slug }).from(products);
+
+		// Query the database for all category slugs
+		const parentCategory = alias(categories, 'parentCategory');
+		const categorySlugs = await db
+			.select({
+				categorySlug: categories.slug,
+				parentSlug: parentCategory.slug
+			})
+			.from(categories)
+			.leftJoin(parentCategory, eq(parentCategory.id, categories.parentId));
+
+		// Generate the URLs for the categories and products
+		const urls = [
+			...productSlugs.map((product) => `/loja/produto/${product.slug}`),
+			...categorySlugs.map((category) => {
+				let url = '/loja/categoria/';
+				if (category.parentSlug) {
+					url += `${category.parentSlug}/`;
+				}
+				url += category.categorySlug;
+				return url;
+			})
+		];
+
+		routes.push(...urls);
+
+		return json(routes);
+	} catch (err) {
+		console.log(err);
+		return error(500, 'Ocorreu um erro ao buscar as rotas.');
+	}
+}) satisfies RequestHandler;
---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,100 @@
+export async function GET({ fetch, setHeaders }) {
+	setHeaders({
+		'Content-Type': 'application/xml',
+		'Cache-Control': 'public, max-age=604800' // on week, in seconds
+	});
+
+	const site = 'https://bioloja.bio.br';
+	const now = new Date();
+	const dateNow = now.toISOString().slice(0, 19) + '+00:00';
+	// now.split('T')[0]
+
+	const response = await fetch('/api/routes');
+	if (!response.ok) {
+		throw new Error('Failed to fetch routes.');
+	}
+	const routes = await response.json();
+
+	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+			xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+			xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+			xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+						http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+<url>
+	<loc>${site}</loc>
+	<lastmod>${dateNow}</lastmod>
+	<changefreq>daily</changefreq>
+	<priority>1.00</priority>
+</url>
+
+${routes
+	.map(
+		(route: string) => `
+<url>
+	<loc>${site}${route}</loc>
+	<changefreq>weekly</changefreq>
+	<lastmod>${dateNow}</lastmod>
+	<priority>0.80</priority>
+</url>
+`
+	)
+	.join('')}
+
+<url>
+  <loc>https://bioloja.bio.br/loja</loc>
+  <lastmod>${dateNow}</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://bioloja.bio.br/carrinho</loc>
+  <lastmod>2023-03-24T18:28:52+00:00</lastmod>
+  <priority>0.40</priority>
+</url>
+<url>
+  <loc>https://bioloja.bio.br/entrar</loc>
+  <lastmod>2023-03-24T18:28:52+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://bioloja.bio.br/cadastrar</loc>
+  <lastmod>2023-03-24T18:28:52+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://bioloja.bio.br/faq</loc>
+  <lastmod>${dateNow}</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://bioloja.bio.br/sobre-nos</loc>
+  <lastmod>${dateNow}</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://bioloja.bio.br/contato</loc>
+  <lastmod>2023-03-24T18:28:52+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://bioloja.bio.br/politica-de-privacidade</loc>
+  <lastmod>2023-03-24T18:28:52+00:00</lastmod>
+  <priority>0.40</priority>
+</url>
+<url>
+  <loc>https://bioloja.bio.br/termos-de-servico</loc>
+  <lastmod>2023-03-24T18:28:52+00:00</lastmod>
+  <priority>0.40</priority>
+</url>
+<url>
+	<loc>https://bioloja.bio.br/esqueci-a-senha</loc>
+	<lastmod>2023-03-24T18:28:52+00:00</lastmod>
+	<priority>0.64</priority>
+</url>
+
+</urlset>
+`;
+
+	return new Response(sitemap);
+}
