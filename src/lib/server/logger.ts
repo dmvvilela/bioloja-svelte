da3e25e8987bd55d04ad7cced089a16f9ec85979
Daniel Vilela <dmvvilela@gmail.com>
2024-05-04 18:44:35 -0300

    removing pino and axiom

--- src/lib/server/logger.ts
+++ src/lib/server/logger.ts
@@ -1,29 +1,35 @@
-import pino from 'pino';
-import { dev } from '$app/environment';
+// import pino from 'pino';
+// import { dev } from '$app/environment';
 
-let logger: pino.Logger;
+// let logger: pino.Logger;
 
-if (dev) {
-	logger = pino({
-		level: 'debug',
-		transport: {
-			target: 'pino-pretty',
-			options: {
-				colorize: true
-			}
-		}
-	});
-} else {
-	logger = pino(
-		{ level: 'info' },
-		pino.transport({
-			target: '@axiomhq/pino',
-			options: {
-				dataset: process.env.AXIOM_DATASET,
-				token: process.env.AXIOM_TOKEN
-			}
-		})
-	);
-}
+// if (dev) {
+// 	logger = pino({
+// 		level: 'debug',
+// 		transport: {
+// 			target: 'pino-pretty',
+// 			options: {
+// 				colorize: true
+// 			}
+// 		}
+// 	});
+// } else {
+// 	logger = pino(
+// 		{ level: 'info' },
+// 		pino.transport({
+// 			target: '@axiomhq/pino',
+// 			options: {
+// 				dataset: process.env.AXIOM_DATASET,
+// 				token: process.env.AXIOM_TOKEN
+// 			}
+// 		})
+// 	);
+// }
+
+// export default logger;
+
+const logger = (err: any) => console.error(err);
+
+logger.error = logger;
 
 export default logger;
