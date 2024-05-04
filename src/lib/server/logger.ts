127279580c1058373696f1274a4c7c161d5c05b3
Daniel Vilela <dmvvilela@gmail.com>
2024-05-04 19:18:20 -0300

    remopving axiom again

--- src/lib/server/logger.ts
+++ src/lib/server/logger.ts
@@ -2,29 +2,32 @@ import pino from 'pino';
 import { dev } from '$app/environment';
 import { AXIOM_DATASET, AXIOM_TOKEN } from '$env/static/private';
 
-let logger: pino.Logger;
+// let logger: pino.Logger;
 
-if (dev) {
-	logger = pino({
-		level: 'debug',
-		transport: {
-			target: 'pino-pretty',
-			options: {
-				colorize: true
-			}
-		}
-	});
-} else {
-	logger = pino(
-		{ level: 'info' },
-		pino.transport({
-			target: '@axiomhq/pino',
-			options: {
-				dataset: AXIOM_DATASET,
-				token: AXIOM_TOKEN
-			}
-		})
-	);
-}
+// if (dev) {
+// 	logger = pino({
+// 		level: 'debug',
+// 		transport: {
+// 			target: 'pino-pretty',
+// 			options: {
+// 				colorize: true
+// 			}
+// 		}
+// 	});
+// } else {
+// logger = pino(
+// 	{ level: 'info' },
+// 	pino.transport({
+// 		target: '@axiomhq/pino',
+// 		options: {
+// 			dataset: AXIOM_DATASET,
+// 			token: AXIOM_TOKEN
+// 		}
+// 	})
+// );
+// }
+
+const logger = (err: any) => console.error(err);
+logger.error = logger;
 
 export default logger;
