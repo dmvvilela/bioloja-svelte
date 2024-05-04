05315f7dcf66dea5a926365fcf5dd9acc19b0aa6
Daniel Vilela <dmvvilela@gmail.com>
2024-05-04 20:06:09 -0300

    now it should work

--- src/lib/server/logger.ts
+++ src/lib/server/logger.ts
@@ -1,33 +1,37 @@
 import pino from 'pino';
 import { dev } from '$app/environment';
-import { AXIOM_DATASET, AXIOM_TOKEN } from '$env/static/private';
+import { AXIOM_DATASET, AXIOM_ORG_ID, AXIOM_TOKEN } from '$env/static/private';
+import { Axiom } from '@axiomhq/js';
 
-// let logger: pino.Logger;
+const logLevels = ['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'silent'];
 
-// if (dev) {
-// 	logger = pino({
-// 		level: 'debug',
-// 		transport: {
-// 			target: 'pino-pretty',
-// 			options: {
-// 				colorize: true
-// 			}
-// 		}
-// 	});
-// } else {
-// logger = pino(
-// 	{ level: 'info' },
-// 	pino.transport({
-// 		target: '@axiomhq/pino',
-// 		options: {
-// 			dataset: AXIOM_DATASET,
-// 			token: AXIOM_TOKEN
-// 		}
-// 	})
-// );
-// }
+let logger: any;
 
-const logger = (err: any) => console.error(err);
-logger.error = logger;
+if (dev) {
+	logger = pino({
+		level: 'debug',
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				colorize: true
+			}
+		}
+	});
+} else {
+	const axiom = new Axiom({
+		orgId: AXIOM_ORG_ID,
+		token: AXIOM_TOKEN
+	});
+
+	logger = Object.fromEntries(
+		logLevels.map((level) => [
+			level,
+			async (message: any) => {
+				axiom.ingest(AXIOM_DATASET, [{ level, message }]);
+				await axiom.flush();
+			}
+		])
+	);
+}
 
 export default logger;
